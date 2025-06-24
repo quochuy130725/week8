5fab01c0f095c4a3bff064114e196fb89641179b
Huy Pham <qhuy8452@gmail.com>
2025-06-24 16:06:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ananya Agrawal</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
